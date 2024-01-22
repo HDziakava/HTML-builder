ea73f7c8f6796e2263188fa507c0f23de62fb6ed
Hanna Dziakava <anne.dyakova@gmail.com>
2024-01-22 22:33:48 +0100

    fix: add callback function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -9,21 +9,21 @@ fs.mkdir(coolPath, { recursive: true }, (err) => {
     return console.error(err);
   }
   console.log('Directory created successfully!');
-});
 
-fs.readdir(folderPath, (err, files) => {
-  files.forEach((file) => {
-    const filePath = path.join(__dirname, `files/${file}`);
+  fs.readdir(folderPath, (err, files) => {
+    files.forEach((file) => {
+      const filePath = path.join(__dirname, `files/${file}`);
 
-    fs.rm(path.join(coolPath, file), (err) => {
-      if (err) {
-        return console.error(err);
-      }
-    });
+      fs.rm(path.join(coolPath, file), (err) => {
+        if (err) {
+          return console.error(err);
+        }
+      });
 
-    fs.copyFile(filePath, path.join(coolPath, file), (err) => {
-      if (err) throw err;
-      console.log(`${file} was copied to 'files-copy'`);
+      fs.copyFile(filePath, path.join(coolPath, file), (err) => {
+        if (err) throw err;
+        console.log(`${file} was copied to 'files-copy'`);
+      });
     });
   });
 });
